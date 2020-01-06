c2900026436eb9d724ad83be3b342b2868380c7d
Felipe <felipecarvalho180@gmail.com>
2020-01-06 20:43:33 -0300

    Implements transition and hover on side icons.

--- nba-web/src/components/sidebar/icon-buttons/IconButtons.js
+++ nba-web/src/components/sidebar/icon-buttons/IconButtons.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { transitions } from 'polished';
 
 import { pointer } from '../../../style/helpers/Helpers';
 import { 
@@ -28,23 +29,45 @@ const Wrapper = styled.div`
   margin-top: 20px;
 `;
 
+const hoverStyle = css`
+  color: red;
+  border: solid 1px red;
+  background-color: white;
+`;
+
 const ICBasketballIcon = styled(BasketballIcon)`
   margin-bottom: 10px;
-  ${ pointer }
+  ${ pointer };
+
+  :hover {
+    ${ hoverStyle };
+  };
 `;
 
 const ICTShirtIcon = styled(TShirtIcon)`
   margin-bottom: 10px;
-  ${ pointer }
+  ${ pointer };
+
+  :hover {
+    ${ hoverStyle }
+  };
 `;
 
 const ICCalendarIcon = styled(CalendarCheckIcon)`
   margin-bottom: 10px;
-  ${ pointer }
+  ${ pointer };
+
+  :hover {
+    ${ hoverStyle }
+  };
 `;
 
 const ICTrophyIcon = styled(TrophyIcon)`
-  ${ pointer }
+  ${ pointer };
+  
+  :hover {
+    ${ hoverStyle }
+  };
 `;
 
 export default IconButtons;
--- nba-web/src/style/icons/Icons.js
+++ nba-web/src/style/icons/Icons.js
@@ -1,5 +1,7 @@
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { transitions } from 'polished';
+
 import { BasketballBall } from 'styled-icons/fa-solid/BasketballBall';
 import { Tshirt } from 'styled-icons/fa-solid/Tshirt';
 import { CalendarCheck } from 'styled-icons/boxicons-solid/CalendarCheck';
@@ -7,38 +9,28 @@ import { Trophy } from 'styled-icons/fa-solid/Trophy';
 
 import { WHITE } from '../variables/Variables';
 
-export const BasketballIcon = styled(BasketballBall)`
+const iconBaseStyle = css`
   height: 20px;
   width: 20px;
   padding: 5px;
   border: 1px solid ${ WHITE };
   border-radius: 5px;
   color: ${ WHITE };
+  ${ transitions(['color', 'border', 'background-color'], 'ease-in-out .5s') };
+`;
+
+export const BasketballIcon = styled(BasketballBall)`
+  ${ iconBaseStyle }
 `;
 
 export const TShirtIcon = styled(Tshirt)`
-  height: 20px;
-  width: 20px;
-  padding: 5px;
-  border: 1px solid ${ WHITE };
-  border-radius: 5px;
-  color: ${ WHITE };
+  ${ iconBaseStyle }
 `;
 
 export const CalendarCheckIcon = styled(CalendarCheck)`
-  height: 20px;
-  width: 20px;
-  padding: 5px;
-  border: 1px solid ${ WHITE };
-  border-radius: 5px;
-  color: ${ WHITE };
+  ${ iconBaseStyle }
 `;
 
 export const TrophyIcon = styled(Trophy)`
-  height: 20px;
-  width: 20px;
-  padding: 5px;
-  border: 1px solid ${ WHITE };
-  border-radius: 5px;
-  color: ${ WHITE };
+  ${ iconBaseStyle }
 `;
