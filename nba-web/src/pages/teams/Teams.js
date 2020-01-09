2721f8b1412297f6c9c7e410ba07fbedd6faa117
Felipe <felipecarvalho180@gmail.com>
2020-01-08 21:25:27 -0300

    Implements teams page background and body font.

--- nba-web/src/pages/teams/Teams.js
+++ nba-web/src/pages/teams/Teams.js
@@ -1,20 +1,29 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import { BLUE, RED, WHITE, BLUE_OPACITY } from '../../style/variables/Variables';
 
 // import { Container } from './styles';
 
 export default function teams() {
   return (
     <Wrapper>
-      <h1>Teams</h1>
+      <Title>Select your team</Title>
     </Wrapper>
   );
-}
+} 
 
 const Wrapper = styled.div`
   display: flex;
   height: 100%;
   justify-content: center;
   align-items: center;
+  background-color: ${ BLUE_OPACITY };
+`;
+
+const Title = styled.div`
+  font-size: 25px;
+  color: ${ RED };
+  text-transform: capitalize;
+  text-shadow: -1px 0 ${ WHITE }, 0 1px ${ WHITE }, 1px 0 ${ WHITE }, 0 -1px ${ WHITE };
 `;
--- nba-web/src/style/variables/Variables.js
+++ nba-web/src/style/variables/Variables.js
@@ -1,6 +1,7 @@
 
 export const RED = '#ED174B';
 export const BLUE = '#006BB7';
+export const BLUE_OPACITY = 'rgba(0, 107, 183, 0.6)';
 export const WHITE = '#FFFFFF';
 export const BLACK = '#2E2E2E';
 
